886a834132e95ee3bf4e1ac9c92b1b8845815f20
My_precious_Vea <keithyvheaiv@gmail.com>
2025-11-09 12:52:46 +0800

    Expenses

--- app/Database/Migrations/2025-11-07-133431_CreateExpensesTable.php
+++ app/Database/Migrations/2025-11-07-133431_CreateExpensesTable.php
@@ -8,11 +8,39 @@ class CreateExpensesTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'expense_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'vendor_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'amount' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'expense_date' => [
+                'type'       => 'DATE',
+                'null'       => false,
+            ],
+            'description' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+        ]);
+
+        $this->forge->addKey('expense_id', true); // Primary key
+        $this->forge->addForeignKey('vendor_id', 'vendors', 'vendor_id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('expenses');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('expenses', true);
     }
 }

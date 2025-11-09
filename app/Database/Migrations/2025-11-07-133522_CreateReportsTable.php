31ced9683d43cee2865d03501dc316787fdd9af8
My_precious_Vea <keithyvheaiv@gmail.com>
2025-11-09 12:58:09 +0800

    Reports

--- app/Database/Migrations/2025-11-07-133522_CreateReportsTable.php
+++ app/Database/Migrations/2025-11-07-133522_CreateReportsTable.php
@@ -8,11 +8,56 @@ class CreateReportsTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'report_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'vendor_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'invoice_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'report_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'debit' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'credit' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'balance' => [
+                'type'       => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => false,
+            ],
+        ]);
+        $this->forge->addKey('report_id', true); // Primary key
+        $this->forge->addForeignKey('vendor_id', 'vendors', 'vendor_id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('invoice_id', 'invoices', 'invoice_id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('reports');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('reports', true);
     }
 }

60ec8b03861b1850c899a841b3e21b4b4c3d604c
My_precious_Vea <keithyvheaiv@gmail.com>
2025-11-09 13:19:48 +0800

    AR_customers and invoice

---
+++ app/Database/Migrations/2025-11-09-051701_CreateCustomersTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'customer_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'customer_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'customer_email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],    
+        ]);
+
+        $this->forge->addKey('customer_id', true); // Primary key
+        $this->forge->createTable('customers');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('customers', true);
+    }
+}
